818aef20d15cc9b45c939694659f0c9374722b32
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-07-03 16:32:56 -0300

    Ajustando css da view que ira gerar o pdf do pedido

--- resources/views/pdf/order.blade.php
+++ resources/views/pdf/order.blade.php
@@ -2,51 +2,58 @@
 <html>
 
 <head>
-    <style>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style type="text/css">
         body {
-            font-family: sans-serif
+            font-family: sans-serif;
         }
 
-        .grid {
-            display: -ms-grid;
-            display: grid;
+        .table {
+            width: 100%;
+            border-collapse: collapse
         }
 
-        .grid-cols-2 {
-            -ms-grid-columns: 1fr 1fr;
-            grid-template-columns: repeat(2, 1fr);
+        .table th,
+        .table td {
+            padding: 0.5rem;
+            border: 1px solid black;
+            text-align: left;
         }
 
-        .grid-cols-4 {
-            -ms-grid-columns: 1fr 1fr 1fr 1fr;
-            grid-template-columns: repeat(4, 1fr);
+        .table td.not-border {
+            border: none;
+            border-right: 1px solid black;
+            border-left: 1px solid black;
         }
 
-        .pr-20 {
-            padding-right: 5rem;
+        .table td.not-border-bottom {
+            border-bottom: none
         }
 
-        .mt-1 {
-            margin-top: 0.25rem;
+        .not-border-td {
+            border: none !important;
+            border-bottom: 1px solid black !important;
         }
 
-        .pl-1 {
-            padding-left: 0.25rem;
+        .border-right {
+            border-right: 1px solid black !important;
         }
 
-        .py-1 {
-            padding-top: 0.25rem;
-            padding-bottom: 0.25rem;
+        .border-left {
+            border-left: 1px solid black !important;
         }
 
-        .border {
-            border-width: 1px;
-            border-style: solid;
-            border-color: black;
+        .border-top {
+            border-top: 1px solid black !important;
         }
 
-        .bg-gray {
-            background-color: rgb(147 168 172);
+        .table td.border-bottom {
+            border-bottom: 1px solid black;
+        }
+
+        .table th {
+            background-color: rgb(147, 168, 172);
+            font-weight: bold;
         }
 
         .font-bold {
@@ -66,53 +73,87 @@
         .mb-4 {
             margin-bottom: 1rem;
         }
+
+        .bg-gray {
+            background-color: rgb(147, 168, 172)
+        }
     </style>
 </head>
 
 <body>
     <div>
         <h1 class="font-bold text-2xl">Pedido - Nº {{ $order->id }}</h1>
-        <p class="bg-gray text-xl font-bold border pl-1">Cliente</p>
-        <div class="grid grid-cols-2 border">
-            <div class="border py-1 pr-20 pl-1">
-                <p class="font-bold">Nome do cliente</p>
-                <p class="mt-1">{{ $order->customer_name }}</p>
-            </div>
-            <div class="border py-1 pr-20 pl-1">
-                <p class="font-bold">Telefone</p>
-                <p class="mt-1">{{ $order->phone_number }}</p>
-            </div>
-            <div class="border py-1 pr-20 pl-1">
-                <p class="font-bold">Endereço</p>
-                <p class="mt-1">{{ $order->address }}</p>
-            </div>
-            <div class="border py-1 pr-20 pl-1">
-                <p class="font-bold">CPF</p>
-                <p class="mt-1">{{ $order->cpf }}</p>
-            </div>
-        </div>
-        <p class="bg-gray text-xl font-bold border pl-1">Itens do pedido</p>
-        <div class="border">
-            <div class="grid grid-cols-4 bg-gray">
-                <p class="font-bold p-1">Nome Produto</p>
-                <p class="font-bold p-1">Quantidade</p>
-                <p class="font-bold p-1">Valor Unitário</p>
-                <p class="font-bold p-1">Valor total</p>
-            </div>
-            @foreach ($order->items as $item)
-                <div class="grid grid-cols-4">
-                    <p class="p-1">{{ $item->name }}</p>
-                    <p class="p-1">{{ $item->pivot_quantity }}</p>
-                    <p class="p-1">R$ {{ number_format((float) $item->pivot_price, 2) }}</p>
-                    <p class="p-1">R${{ number_format((float) $item->pivot_price * $item->pivot_quantity, 2) }}</p>
-                </div>
-            @endforeach
-        </div>
-        <div class="grid grid-cols-2 bg-gray pl-1 gap-x-20 rounded-b py-2">
-            <p><span class="font-bold">Data do pedido:</span> {{ $order->created_at }}</p>
-            <p><span class="font-bold">Valor total:</span> R${{ number_format((float) $order->total_amount, 2) }}</p>
-            <p><span class="font-bold">Contato:</span> (16) 99613-1443</p>
-        </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th colspan="2" class="bg-gray text-xl font-bold border pl-1">Cliente</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="not-border-bottom font-bold">Nome do cliente</td>
+                    <td class="not-border-bottom font-bold">Telefone</td>
+                </tr>
+                <tr>
+                    <td class="not-border">{{ $order->customer_name }}</td>
+                    <td class="not-border">{{ $order->phone_number }}</td>
+                </tr>
+                <tr>
+                </tr>
+                <tr>
+                    <td class="not-border-bottom font-bold">Endereço</td>
+                    <td class="not-border-bottom font-bold">CPF</td>
+                </tr>
+                <tr>
+                    <td class="not-border border-bottom">{{ $order->address }}</td>
+                    <td class="not-border border-bottom">{{ $order->cpf }}</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <p class="bg-gray font-bold " style="padding: 0.25rem;">Itens do pedido</p>
+        <table class="table">
+
+            <thead>
+                <tr>
+                    <th class="bg-gray font-bold p-1 not-border-td border-left border-top">
+                        Nome Produto</th>
+                    <th class="bg-gray font-bold p-1 not-border-td border-top">Quantidade
+                    </th>
+                    <th class="bg-gray font-bold p-1 not-border-td border-top">Valor Unitário
+                    </th>
+                    <th class="bg-gray font-bold p-1 not-border-td border-right border-top">
+                        Valor total</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($order->items as $item)
+                    <tr>
+                        <td class="not-border-td border-left">{{ $item->name }}</td>
+                        <td class="not-border-td">{{ $item->getOriginal()['pivot_quantity'] }}</td>
+                        <td class="not-border-td">R$ {{ number_format((float) $item->getOriginal()['pivot_price'], 2) }}
+                        </td>
+                        <td class="not-border-td border-right">
+                            R${{ number_format((float) $item->getOriginal()['pivot_price'] * (int) $item->getOriginal()['pivot_quantity'], 2) }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <table class="table">
+            <tbody>
+                <tr>
+                    <td colspan="2" class="bg-gray pl-1 gap-x-20 rounded-b py-2">
+                        <p><span class="font-bold">Data do pedido:</span> {{ $order->created_at }}</p>
+                        <p><span class="font-bold">Valor total:</span> R$
+                            {{ number_format((float) $order->total_amount, 2) }}</p>
+                        <p><span class="font-bold">Contato:</span> (16) 99613-1443</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 
